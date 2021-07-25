33c3a28cf806eae0bf3e9a1bd9c34c9178ca4bf4
gabengar <gabengar@protonmail.com>
2021-07-25 16:00:13 +0500

    about page

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import { PageProps } from '#classes/props.js';
 const router = express.Router();
 
-/** 
- * GET home page.
- */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// GET home page.
+router.get('/', (req, res, next) => {
+  res.render('index.pug', { title: 'Express' });
 });
+
+// GET about
+router.get("/about", (req, res, next) => {
+  const props = new PageProps("About me");
+  res.render("about.pug", {
+    props
+  });
+})
 
 export default router;
